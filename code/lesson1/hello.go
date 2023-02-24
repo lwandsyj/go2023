bf53814bbaa96f9cde5c0d5ebc064cdb0cb063e1
lwaisyj <lwaisyj@163.com>
2023-02-24 12:47:37 +0800

    基础

---
+++ code/lesson1/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a = "10"
+	fmt.Println("hellog go {0}")
+
+}
---
+++ code/lesson1/hello2.go
@@ -0,0 +1,18 @@
+package hello
+
+func Hellos(names []string) (map[string]string, error) {
+	// A map to associate names with messages.
+	messages := make(map[string]string)
+	// Loop through the received slice of names, calling
+	// the Hello function to get a message for each name.
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		// In the map, associate the retrieved message with
+		// the name.
+		messages[name] = message
+	}
+	return messages, nil
+}
